491c1431e004086ef0236c305f551e9f070967a3
Tyler <trcave@gmail.com>
2021-02-16 22:34:35 -0600

    First commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let cpuWeapon;
-let playerWeapon;
-let cpuCount = 0;
+let cpuWeapon = '';
+let playerChoice = '';
 let playerCount = 0;
+let cpuCount = 0;
+
+const btn = document.querySelectorAll('button');
+const weapons = document.getElementsByClassName('weapon');
+const result = document.getElementById('result');
+const score = document.getElementById('score');
+const reload = document.getElementById('reload');
+
+Array.from(weapons).forEach((weapon) => {
+    weapon.addEventListener('click', (e) => {
+        playerChoice = weapon.id; cpuSelect(); playRound(playerChoice, cpuWeapon);
+    });
+})
 
 function cpuSelect() {
     i = Math.floor((Math.random()) * 3) + 1;
@@ -21,27 +33,28 @@ function cpuSelect() {
     return cpuWeapon;
 }
 
-
 function playRound(playerWeapon, cpuWeapon) {
-    playerWeapon = prompt('Enter rock, paper, or scissors: ')
-    playerWeapon = playerWeapon.toLowerCase();
-    if (playerWeapon === cpuWeapon) {return 'It\'s a draw!!! ' + playerWeapon + ' & ' + cpuWeapon + ' are twins.'}
-    else if (playerWeapon == 'rock' && cpuWeapon == 'paper') {cpuCount++; return 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
-    else if (playerWeapon == 'scissors' && cpuWeapon == 'rock') {cpuCount++; return 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
-    else if (playerWeapon == 'paper' && cpuWeapon == 'scissors') {cpuCount++; return 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
-    else {playerCount++; return 'You win! ' + playerWeapon + ' beats ' + cpuWeapon};
+    if (playerWeapon === cpuWeapon) {cpuCount++; playerCount++; result.textContent = 'It\'s a draw!!! You both chose ' + playerWeapon + '.'}
+    else if (playerWeapon == 'rock' && cpuWeapon == 'paper') {cpuCount++; result.textContent = 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
+    else if (playerWeapon == 'scissors' && cpuWeapon == 'rock') {cpuCount++; result.textContent = 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
+    else if (playerWeapon == 'paper' && cpuWeapon == 'scissors') {cpuCount++; result.textContent = 'You lose! ' + cpuWeapon + ' beats ' + playerWeapon}
+    else {playerCount++; result.textContent = 'You win! ' + playerWeapon + ' beats ' + cpuWeapon}
+
+    score.innerHTML = 'Computer: ' + cpuCount + '<br>' + 'Player: ' + playerCount   
+
+    if (cpuCount === 5) {
+        result.innerText = 'YOU LOSE!';
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].classList.add('disabled');
+        }
+        reload.innerHTML = '<a href="#" onclick="window.location.reload(true);">Try again?</a>'
+
+    } else if (playerCount === 5) {
+        result.innerText = 'Congratulations!! You beat the computer.';
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].classList.add('disabled');
+        }
+        reload.innerHTML = '<a href="#" onclick="window.location.reload(true);">Try again?</a>'
+    }
+    
 }
-
-function playGame() {
-    while (playerCount < 5 && cpuCount < 5) {
-        alert(playRound(playerWeapon, cpuSelect()));
-        alert('Player score: ' + playerCount + ' | ' + 'Computer score: ' + cpuCount)
-    }
-    if (playerCount > cpuCount) {
-        alert('Victorious!')
-    } else {
-        alert('You lose!!! Good DAY sir!');
-    }
-}   
-
-playGame();
